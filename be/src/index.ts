5ebe08b758cc7d02f9512784ae165acd3ab1f200
Aarav Shukla <aarav.shukla105@gmail.com>
2025-02-03 03:34:47 +0530

    Update/Added the remaining files of the application

--- be/src/index.ts
+++ be/src/index.ts
@@ -5,13 +5,13 @@ import Anthropic from '@anthropic-ai/sdk';
 const anthropic = new Anthropic();
 
 async function main(){
-    const msg = await anthropic.messages.create({
-        model: "claude-3-5-sonnet-20241022",
+    anthropic.messages.stream({
+        messages: [{role: 'user', content: "Hello"}],
+        model: 'claude-3-5-sonnet-20241022',
         max_tokens: 1024,
-        temperature: 0,
-        messages: [],
-      });
-      console.log(msg);
-      
+    }).on('text', (text) => {
+        console.log(text);
+    });
+
 }
 main();
